c3e323d499fd49cadb66b4b818318d4135a4f39e
ckashby <clay.ashby@gmail.com>
2021-01-04 12:36:14 -0800

    Added Home, About and Contact pages and routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 // import ShowContextData from "./ShowContextData";
 // import UpdateContextData from "./UpdateContextData";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Pages...</h2>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/" exact component={HomePage} />
+      </div>
       {/* <section>
         <ShowContextData />
         <hr />
         <UpdateContextData />
       </section> */}
-    </div>
+    </BrowserRouter>
   );
 }
 
